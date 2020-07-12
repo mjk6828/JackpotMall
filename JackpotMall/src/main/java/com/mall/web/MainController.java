bdd23e95816c51166dd1294fd2c6541d84cf0a28
mjk6828 <59157558+mjk6828@users.noreply.github.com>
2020-07-13 00:59:46 +0900

    셋팅2

--- JackpotMall/src/main/java/com/mall/web/MainController.java
+++ JackpotMall/src/main/java/com/mall/web/MainController.java
@@ -3,12 +3,15 @@ package com.mall.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Controller
 public class MainController {
 
 	@RequestMapping("Main.do")
 	public String Main() {
-		
+		log.info("Main");
 		return "Main";
 	}
 }

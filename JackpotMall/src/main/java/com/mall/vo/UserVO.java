c0c038f810dc23a12e6090be28a44ca5854a35b3
mjk6828 <59157558+mjk6828@users.noreply.github.com>
2020-07-27 21:34:19 +0900

    maria db setting

--- JackpotMall/src/main/java/com/mall/vo/UserVO.java
+++ JackpotMall/src/main/java/com/mall/vo/UserVO.java
@@ -8,6 +8,7 @@ import lombok.ToString;
 @Getter@Setter@ToString
 public class UserVO {
 
+	private int usernum;
 	private String userid;
 	private String userpass;
 	private String userpasscheck;
--- JackpotMall/src/main/java/com/mall/web/LoginController.java
+++ JackpotMall/src/main/java/com/mall/web/LoginController.java
@@ -1,10 +1,15 @@
 package com.mall.web;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.mall.service.UserService;
 import com.mall.vo.UserVO;
 
 import lombok.extern.log4j.Log4j2;
@@ -13,30 +18,37 @@ import lombok.extern.log4j.Log4j2;
 @Controller
 public class LoginController {
 
-	@RequestMapping(value="/login.do")
-	public String Login() throws Exception{
-		
+	@Resource(name = "UserService")
+	private UserService userService;
+
+	@RequestMapping(value = "/login.do")
+	public String Login() throws Exception {
+
+		List<UserVO> vo = userService.userlist();
+
+		log.info(vo.toString());
+		log.info(vo.size());
+
 		return "login/login";
 	}
-	
-	@RequestMapping(value="LoginForm.do", method=RequestMethod.POST)
-	public String LoginForm(Model model, UserVO vo) throws Exception{
-		
+
+	@RequestMapping(value = "LoginForm.do", method = RequestMethod.POST)
+	public String LoginForm(Model model, UserVO vo) throws Exception {
+
 		log.info(vo.toString());
 		return "redirect:Main.do";
 	}
-	
-	
-	@RequestMapping(value="/Signin.do", method=RequestMethod.GET)
-	public String Signin() throws Exception{
-		
+
+	@RequestMapping(value = "/Signin.do", method = RequestMethod.GET)
+	public String Signin() throws Exception {
+
 		return "login/signin";
 	}
-	
-	@RequestMapping(value="/SignUp.do", method=RequestMethod.POST)
-	public String Singup(Model model, UserVO vo) throws Exception{
-	
+
+	@RequestMapping(value = "/SignUp.do", method = RequestMethod.POST)
+	public String Singup(Model model, UserVO vo) throws Exception {
+
 		log.info(vo.toString());
-	return "redirect:Main.do";
+		return "redirect:Main.do";
 	}
 }

8a56fd8a0d969d42a3979ca3ddda134c74a45c94
DESKTOP-95BUHFS\Jinhyeok <qwedf823>
2020-08-16 11:05:33 +0900

    ㅊ

--- JackpotMall/src/main/java/com/mall/vo/UserVO.java
+++ JackpotMall/src/main/java/com/mall/vo/UserVO.java
@@ -16,21 +16,29 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class UserVO {
+	/** ID */
 	@Id @GeneratedValue
 	private long id;
 	
+	/** 이름 */
 	@NotEmpty
 	private String name;
 	
+	/** 암호 */
 	@NotEmpty
 	private String password;
 	
+	/** 주소 */
 	private String address;
+	/** 이메일 */
 	private String email;
+	/** 핸드폰 번호 */
 	private String phone;
 	
+	/** 가입일 */
 	@NotEmpty
 	private Date regidate;
 	
+	/** 권한 */
 	private char verify;
 }
--- JackpotMall/src/main/java/com/mall/web/LoginController.java
+++ JackpotMall/src/main/java/com/mall/web/LoginController.java
@@ -22,27 +22,42 @@ public class LoginController {
 	@Resource(name = "UserService")
 	private UserService userService;
 	
+	/**
+	 * 로그인 페이지로 이동
+	 * @param request - 세션 확인을 위한 매개변수
+	 * @return 세션이 있으면 메인 화면 없으면 로그인 화면
+	 * @exception Exception
+	 */
 	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
-	public String Login(Model model, HttpServletRequest request) throws Exception {
-		/* 세션이 있으면 로그인 페이지로 이동*/
+	public String Login(HttpServletRequest request) throws Exception {
 		HttpSession session = request.getSession();
 		
+		/* 세션이 있으면 로그인 페이지로 이동*/
 		if(session.getAttribute("uSession") != null)
 			return "redirect:Main.do";
 		
 		return "login/login";
 	}
-	// Login
+	/**
+	 * 로그인 확인
+	 * @param request - 세션 확인을 위한 매개변수
+	 * @param vo - 사용자의 로그인값을 받음
+	 * @return 값이 있으면 메인 화면 아니면 로그인 화면
+	 * @exception Exception
+	 */
 	@RequestMapping(value = "LoginForm.do", method = RequestMethod.POST)
 	public String LoginForm(HttpServletRequest request, UserVO vo) throws Exception {
 		HttpSession session = request.getSession();
 		UserVO user = null;
 		
+		/* 조회한 값이 존재하지 않을 경우 */
 		if((user = userService.logincheck(vo)) == null)
 			return "redirect:login.do";
+		/* 세션 등록 */
 		session.setAttribute("uSession", user);
 		return "redirect:Main.do";
 	}
+	
 	@RequestMapping(value ="Logout.do", method = RequestMethod.GET)
 	public String Logout(HttpServletRequest request) throws Exception{
 		HttpSession session = request.getSession();

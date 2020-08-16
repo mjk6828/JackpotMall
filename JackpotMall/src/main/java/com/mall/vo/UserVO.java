2d49c7c58474e3ea6562cbd3f48c0bef4e162353
DESKTOP-95BUHFS\Jinhyeok <qwedf823>
2020-08-16 10:56:45 +0900

    s

--- JackpotMall/src/main/java/com/mall/vo/UserVO.java
+++ JackpotMall/src/main/java/com/mall/vo/UserVO.java
@@ -3,26 +3,34 @@ package com.mall.vo;
 
 import java.sql.Date;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-@Getter@Setter@ToString
+import org.springmodules.validation.bean.conf.loader.annotation.handler.NotEmpty;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class UserVO {
-	//사용자 넘버
-	private int id;
-	//사용자 이름
+	@Id @GeneratedValue
+	private long id;
+	
+	@NotEmpty
 	private String name;
-	//사용자 암호
+	
+	@NotEmpty
 	private String password;
-	//사용자 주소
+	
 	private String address;
-	//사용자 이메일
 	private String email;
-	//사용자 폰 번호
 	private String phone;
-	//가입일
+	
+	@NotEmpty
 	private Date regidate;
-	//권한
+	
 	private char verify;
 }
